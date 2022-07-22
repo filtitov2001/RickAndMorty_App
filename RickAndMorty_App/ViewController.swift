7c2f11a975b6da98e2a098bd3d3cc67bda9d7e77
Felix Titov <titow.feliks@yandex.ru>
2022-07-22 20:59:14 +0300

    Initial Commit

---
+++ RickAndMorty_App/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  RickAndMorty_App
+//
+//  Created by Felix Titov on 7/22/22.
+//  Copyright © 2022 by Felix Titov. All rights reserved.
+//  
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
